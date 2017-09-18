09bf919792e0743901e9cba187a3b4ec4a34d63d
jumpman24 <oleksandr.hiliazov@gmail.com>
2017-09-19 00:10:09 +0300

    addded config file

---
+++ config.py
@@ -0,0 +1,34 @@
+import sgfanalyze_ray
+import os
+
+config = {
+    'leela_executable': "Leela0110GTP.exe",
+    'save_to_file': sgfanalyze_ray.args.SGF_FILE.split('.')[0] + '_analyzed.sgf',
+
+    # Display analysis on moves losing approx at least this much win rate when the game is close
+    'analyze_threshold': 0.050,
+    # Explore variations on moves losing approx at least this much win rate when the game is close
+    'variations_threshold': 0.050,
+    # How many seconds to use per search?
+    'seconds_per_search': 15,
+    # How many nodes to explore in each variation tree
+    'nodes_per_variation': 3,
+    # Number of moves to show from the sequence of suggested moves
+    'num_to_show': 3,
+    # Build pdf graph of win rate, must have matplotlib installed
+    'win_graph': False,
+    # Remove existing comments from the main line of the SGF file
+    'wipe_comments': False,
+    # Analyze game starting from one move till another move
+    'analyze_start': 0,
+    'analyze_end': 1000,
+    # Verbosity
+    'verbosity': 0,
+    # Number of restarts when bot crashes
+    'restarts': 1,
+    # Path to directory to cache partially complete analyses
+    'ckpt_dir': os.path.expanduser('~/.leela_checkpoints'),
+    # Skip analysis of white and black
+    'skip_white': False,
+    'skip_black': False
+}
